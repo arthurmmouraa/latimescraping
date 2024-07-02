fb32fa2451b00d6023097868a577c668b0d60fc2
Arthur Marques <arthur.m.moura@outlook.com>
2024-07-02 19:39:38 -0300

    Debug.log path updated

--- config/logging_config.py
+++ config/logging_config.py
@@ -9,6 +9,6 @@ def configure_logger():
         logging.Logger: Configured logger object.
     """
     logger.add(
-        "debug.log", rotation="1 MB"
+        "output/debug.log", rotation="1 MB"
     )  # Save all the logs in an archive. Rotation happens when it gets at 1 MB.
     return logger
